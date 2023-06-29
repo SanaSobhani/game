3d823b758a38727bf826723379b5af0eecebfda6
SanaSobhani <sanaacc26@gmail.com>
2023-06-30 03:42:42 +0430

    editing photos of heroes

--- finalGame/src/main/java/com/example/finalgame/building/WatchTower.java
+++ finalGame/src/main/java/com/example/finalgame/building/WatchTower.java
@@ -2,8 +2,8 @@ package com.example.finalgame.building;
 
 import com.example.finalgame.HelloApplication;
 
-public class WatchTower extends NonDefensiveBuilding{
+public class WatchTower extends DefensiveBuilding{
     public WatchTower(){
-        super(4,new HelloApplication().getClass().getResource("B.watchTower.png").toString());
+        super(4,6,new HelloApplication().getClass().getResource("B.watchTower.png").toString());
     }
 }
