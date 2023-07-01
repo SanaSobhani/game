f55342dec50d6396f38f13313ba6966ac079b7ac
SanaSobhani <sanaacc26@gmail.com>
2023-07-01 20:09:16 +0430

    choosing map page

---
+++ finalGame/src/main/java/com/example/finalgame/pages/AllMaps.java
@@ -0,0 +1,102 @@
+package com.example.finalgame.pages;
+
+import com.example.finalgame.map.Map1;
+import com.example.finalgame.map.Map2;
+import com.example.finalgame.map.Map3;
+import com.example.finalgame.map.Map4;
+import com.example.finalgame.player.Player;
+import com.example.finalgame.player.PlayerController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+import java.sql.SQLException;
+
+public class AllMaps {
+
+    @FXML
+    private AnchorPane ancherPane;
+
+    @FXML
+    private ImageView backGround;
+
+    @FXML
+    private Text titleTxt;
+
+    @FXML
+    private ImageView map2;
+
+    @FXML
+    private ImageView map1;
+
+    @FXML
+    private ImageView map3;
+
+    @FXML
+    private ImageView map4;
+
+    @FXML
+    private Text defensiveTxt;
+
+    @FXML
+    private Text non_defensiveTxt;
+
+    @FXML
+    private Text number4;
+
+    @FXML
+    private Text number3;
+
+    @FXML
+    private Text number2;
+
+    @FXML
+    private Text numberr1;
+
+    @FXML
+    private Text number5;
+
+    @FXML
+    private Text number6;
+
+    @FXML
+    private Text number7;
+
+    @FXML
+    private Text number8;
+
+    @FXML
+    private Button selectButton1;
+
+    @FXML
+    private Button selectButton2;
+
+    @FXML
+    private Button selectButton3;
+
+    @FXML
+    void selection1(ActionEvent event) throws SQLException {
+        Player.dataBase.saveMapToDataBase(Map2.getMap2().getId(), PlayerController.getPassWord());
+    }
+
+    @FXML
+    void selection2(ActionEvent event) throws SQLException {
+        Player.dataBase.saveMapToDataBase(Map3.getMap3().getId(), PlayerController.getPassWord());
+
+    }
+
+    @FXML
+    void selection3(ActionEvent event) throws SQLException {
+        Player.dataBase.saveMapToDataBase(Map4.getMap4().getId(), PlayerController.getPassWord());
+
+    }
+
+    @FXML
+    void selection4(ActionEvent event) throws SQLException {
+        Player.dataBase.saveMapToDataBase(Map1.getMap1().getId(), PlayerController.getPassWord());
+    }
+
+}
